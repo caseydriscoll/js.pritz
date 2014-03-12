feccb9c0f018233a5bc5625a80858fc4d14f6d03
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2014-03-12 22:05:20 +0000

    Adds simple start/stop functionalilty

--- js/script.js
+++ js/script.js
@@ -1,11 +1,38 @@
 $(document).ready(function() {
+  var words;
+
   $("#button").click(function() {
     $.ajax({
       url : "docs/tale-of-two-cities.txt",
       dataType: "text",
       success : function (data) {
         $(".text").html(data);
+        words = $(".text").text().split(' ');
       }
     });
   });
+
+  $("#nextword").click(function() {
+    nextWord();
+  });
+
+  $('#stop').hide();
+
+  var intervalID;
+  $("#start").click(function() {
+    $(this).hide();
+    $('#stop').show();
+    intervalID = setInterval(nextWord, 500);
+  });
+
+  $("#stop").click(function() {
+    $(this).hide();
+    $('#start').show();
+    clearInterval(intervalID);
+  });
+
+  function nextWord(){
+    var word = words.pop();
+    $(".word").empty().html(word);
+  }
 });
