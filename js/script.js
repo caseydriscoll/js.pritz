c0b7ec388c3a861b41650ed60b1e542a470797ee
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2014-03-12 21:28:20 +0000

    Adds basic styling, markup and scripting

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#button").click(function() {
+    $.ajax({
+      url : "docs/tale-of-two-cities.txt",
+      dataType: "text",
+      success : function (data) {
+        $(".text").html(data);
+      }
+    });
+  });
+});
