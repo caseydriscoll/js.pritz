4eb7e31eca33410e59504ab323ee2a18caba3696
Casey Patrick Driscoll <caseypatrickdriscoll@me.com>
2014-03-13 11:20:35 +0000

    Adds math for shifting word to middle. Pretty close atleast. Good enough for now.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
   var words;
   var nextWord;
+  var wordCount;
+  var wordTimer;
+
+  var charWidths = [27, 30, 28, 28, // a, b, c, d
+                    28, 16, 28, 28, // e, f, g, h
+                    12, 12, 28, 12, // i, j, k, l
+                    29, 28, 28, 30, // m, n, o, p
+                    28, 18, 25, 18, // q, r, s, t
+                    28, 29, 38, 29, // u, v, w, x
+                    29, 29];            // y, z
 
   $("#button").click(function() {
     $.ajax({
@@ -27,6 +37,8 @@ $(document).ready(function() {
   $("#start").click(function() {
     $(this).hide();
     $('#stop').show();
+    wpm = $('#rate').val();
+    speed = 60000 / wpm;
     intervalID = setInterval(run, speed);
   });
 
@@ -40,7 +52,7 @@ $(document).ready(function() {
     clearInterval(intervalID);
     
     if ( hasPunct )
-      speed = speed * 3;
+      speed = speed * 2;
    else
       speed = 60000 / wpm; 
 
@@ -53,7 +65,23 @@ $(document).ready(function() {
     hasPunct = false;
     var word = nextWord;
     nextWord = words.pop();
-    var i = word.length / 2;
+    var i = Math.floor(word.length / 2);
+
+    // shift is every word before the midchar plus half the midchar
+    var shift = 0; 
+    for ( var j = 0; j < i; j++) {
+      if ( word.charCodeAt(j) >= 97 && word.charCodeAt(j) < 123 ) {
+        shift += charWidths[word.charCodeAt(j) - 97];
+        //console.log("word: " + word + " i/j: " + i+"/"+j + " charCodeAt: " + word.charCodeAt(j) + " shift: " + shift);
+      } else {
+        shift += 30;
+        //console.log("word: " + word + " i/j: " + i+"/"+j + " charCodeAt: " + word.charCodeAt(j) + " shift: " + shift);
+      }
+    }
+    shift += charWidths[word.charCodeAt(i) - 97] / 2;
+    shift = 0 - shift;
+
+
     var midchar = word.substring( i, i + 1);
     midchar = "<span>" + midchar + "</span>";
     word = word.substring(0, i) + midchar + word.substring(i + 1);
@@ -62,6 +90,6 @@ $(document).ready(function() {
     if ( endchar == '.' || endchar == ',' )
       hasPunct = true;
 
-    $(".word").empty().html(word);
+    $(".word").empty().css("margin-left", shift + "px").html(word);
   }
 });
